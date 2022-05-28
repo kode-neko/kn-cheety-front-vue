0c8c48830f516dd7e26c8fb4406cbe5537766c3b
kodeneko <ladysun.freedom@gmail.com>
2022-05-28 02:38:18 +0200

    [dev] box article

---
+++ src/boxes/BoxArticle.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+BoxBase(:title="title")
+  div {{ content }}
+  div(:class="$style.footer")
+    div(:class="$style.left")
+      TagInfo(
+        v-for="tag in tagList",
+        :key="tag.id",
+        :label="tag.label",
+        :type="'a'",
+        :isDeletable="false",
+        @delete="() => deleteTag(tag)"
+      )
+    div(:class="$style.right")
+      BtnIcon(:icon="['fa', 'trash-can']", size="sm", type="b")
+      BtnIcon(:icon="['fas', 'pencil']", size="sm", type="b")
+</template>
+
+<script lang="ts">
+import BoxBase from "./BoxBase.vue";
+import { BtnIcon } from "../components/btn";
+import TagInfo from "../components/TagInfo.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    BoxBase,
+    BtnIcon,
+    TagInfo,
+  },
+  props: {
+    title: String,
+    content: String,
+    tagList: Array,
+  },
+  setup(props, p) {
+    return {};
+  },
+});
+</script>
+
+<style lang="stylus" module>
+@import "../assets/base"
+.footer
+  display flex
+  justify-content space-between
+  margin-top pd-md
+  .right
+      display flex
+      gap pd-sm
+</style>
